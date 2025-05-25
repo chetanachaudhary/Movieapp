3dacc50d5d48f6e7e5dac7de6d8940891c7cfca0
chetanachaudhary <chetanachaudhary445@gmail.com>
2025-05-25 17:37:54 +0545

    save page design

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 const save = () => {
   return (
-    <View>
-      <Text>save</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <FontAwesome6 name="save" size={24} color="white" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default save
+export default save;
